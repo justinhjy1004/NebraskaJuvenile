8388724325aac9dc422a2e282dbae580ab16aeb8
justinhjy1004 <justinhjy1004@gmail.com>
2025-03-22 08:23:06 -0400

    changes

--- script.R
+++ script.R
@@ -2,23 +2,39 @@ library(tidyverse)
 
 df <- read_csv("Counts.csv")
 
-ezaco <- read_csv("EZACO.csv") |>
-  gather( key = "Classification", value = "count", 3:5) |>
-  drop_na() |>
-  mutate( Source = "EZACO")
+scone <- read_csv("SCONE.csv") |>
+  gather(key = "Classification", value = "count", 3:5) |>
+  mutate(Source = "SCONE")
 
 df |>
-  mutate( Classification = ifelse( Classification == "Felony" | Classification == "Misdemeanor", "Delinquency", Classification)) |>
-  group_by(Classification, Year, County) |>
-  summarize(count = sum(count)) |>
-  filter(Classification != "Traffic Offense") -> df
-
-df |> mutate( Source = "NE Justice") |> rbind(ezaco) -> df
-
+  mutate( Source = "NE Justice" ) |>
+  filter(Classification != "Traffic Offense" ) |>
+  filter(Classification != "Dependency") |>
+  rbind(scone)  -> df
 
 ggplot(df, aes( x = Year, y = count, color = Classification)) +
   geom_point(aes(shape = Source)) +
   geom_line(aes(linetype = Source)) +
-  facet_wrap(~ County) +
+  facet_wrap(~ County + Classification, scales = "free_y") +
   theme_classic() +
-  theme(legend.position = "top")
+  theme(legend.position = "top") +
+  ylab("Count") +
+  scale_x_continuous(breaks=seq(2010,2024,3))
+
+ggsave("SCONE_v_NE_Justice.png", device = "png", width = 9, height = 5)
+
+df |>
+  spread(key = "Source", value = "count") |>
+  drop_na() |>
+  mutate(Difference = SCONE - `NE Justice`)-> d_diff
+
+
+ggplot(d_diff, aes( x = Year, y = Difference, color = Classification)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~ County + Classification, scales = "free_y") +
+  theme_classic() +
+  theme(legend.position = "top") +
+  scale_x_continuous(breaks=seq(2015,2024,3))
+
+ggsave("SCONE_v_NE_Justice_Difference.png", device = "png", width = 9, height = 5)
